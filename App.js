2537a42bdc8150ca228c416faf56833cc7952b60
alestor123 <alestoraldous@gmail.com>
2020-12-30 22:10:08 +0530

    added todays dt

--- App.js
+++ App.js
@@ -3,6 +3,7 @@
 var Box = require("cli-box"),
 figlet = require('figlet'),
 newyearDate = new Date(`Jan 1`),
+now = new Date().toJSON().slice(5,10),
 chalk = require('chalk'); 
 
 function Title(message){
