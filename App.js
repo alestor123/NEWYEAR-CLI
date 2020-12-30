62c85eada227ad295848b1013c79d2bc881b3e66
alestor123 <alestoraldous@gmail.com>
2020-12-30 21:53:59 +0530

    added title

--- App.js
+++ App.js
@@ -3,3 +3,9 @@
 var Box = require("cli-box"),
 figlet = require('figlet'),
 chalk = require('chalk'); 
+
+function Title(message){
+figlet(message, (err, data) => {
+if (err) throw err     
+console.log(chalk.red(message))
+})}
